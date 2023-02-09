970c628dfa8b7b09901cedc31532c48a42ab2ed7
Kellan Anderson <kellandanderson@gmail.com>
2023-02-09 15:21:16 -0500

    added python virtual enviroment script

--- virtualenv.sh
+++ virtualenv.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 # Script to help manage python virtual enviroments
+
+STATUS_INVALID_USE=1
+
+if [[ $# == 0 ]]; then
+    v_status=`. .venv/bin/activate`
+    if [[ $? != 0 ]]; then
+        echo "No virtual enviroment found, would you like to make one? [y/n]"
+        read answer
+        if [[ $answer == "y" ]] || [[ $answer == "yes" ]]; then
+            python3 -m venv .venv
+        elif [[ $answer != "n" ]] && [[ $answer != "no" ]]; then
+            echo "Unrecoknized answer"
+            exit $STATUS_INVALID_USE
+        fi
+    fi
+fi
